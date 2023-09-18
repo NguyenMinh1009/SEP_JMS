9db7fda0efecd51a0878b9e46c4389e769676a07
Cloudy_Place <minhnnm1009@gmail.com>
2023-09-18 15:46:02 +0700

    setup lib, env

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import pluginRewriteAll from "vite-plugin-rewrite-all";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+//   plugins: [react()],
+//   define: {
+//     'process.env': process.env
+//   }
+// })
+
+export default ({ mode }) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    // To access env vars here use process.env.TEST_VAR
+    base: "./",
+    server: {
+      host: true
+    },
+    plugins: [pluginRewriteAll()]
+  });
+};

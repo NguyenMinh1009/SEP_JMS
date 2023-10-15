8800f1d46c95668e7d58f7077d67ac13886fcff8
Cloudy_Place <minhnnm1009@gmail.com>
2023-10-15 20:25:16 +0700

    commit fix bug

--- SEP_JMS.UI/src/App.tsx
+++ SEP_JMS.UI/src/App.tsx
@@ -27,7 +27,7 @@ import Home from "./pages/Home";
 
 import CreateSubTask from "./components/ProjectManagement/SubTasks/CreateSubTask";
 // import SubTaskDetail from "./pages/SubTaskDetail";
-import Profile from "./pages/Profile";
+import Profile from "./pages/profile";
 import ForgotPassword from "./pages/ForgotPassword";
 
 const theme = createTheme(
